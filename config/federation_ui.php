397b615ad73b20e2ea964f72924d50202027124d
Nghokeng Daniel <nghokend@gmail.com>
2024-05-23 11:11:24 +0100

    update default cdn address

--- config/federation_ui.php
+++ config/federation_ui.php
@@ -2,10 +2,10 @@
 
 return [
     "server" => [
-        "host" => env("FEDERATION_SERVER_HOST", "localhost"),
+        "host" => env("FEDERATION_SERVER_HOST", "0.0.0.0"),
         "port" => env("FEDERATION_SERVER_PORT", 8142)
     ],
     "cdn" => [
-        "url" => env('FEDERATION_CDN_URL', "http://localhost:8142")
+        "url" => env('FEDERATION_CDN_URL', "http://0.0.0.0:8142")
     ]
 ];
